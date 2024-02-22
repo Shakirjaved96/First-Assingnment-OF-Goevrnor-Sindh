bdec15ba80f282ecab18613799cd3fff298e7b7f
Shakir Hussain <shakirjaved727@gmail.com>
2024-02-23 01:28:51 +0500

    solve some issue

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 // // personal Message: Store a person’s name in a variable, and print a message to that person.
 // // our message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 // let personName: string = "Eric";
-// console.log('Hello $(personName), would you like to learn some Python today?' );
+// console.log(`Hello ${personName}, would you like to learn some Python today?`);
 
 
 // // task 3
